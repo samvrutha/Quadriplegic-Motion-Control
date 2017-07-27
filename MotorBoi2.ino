9a763834827ce081df31b81c5fb4cf3a580b2bd5
Samvruta Tumuluru <tsamvrutha@gmail.com>
2017-07-27 11:27:35 -0500

    Final Version of Transmit and Receive

---
+++ MotorBoi2.ino
@@ -0,0 +1,85 @@
+#include <ZumoMotors.h>
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(0, 1);
+
+
+ZumoMotors motors;
+int Back = 1;
+int Forward = 2;
+int Right = 3;
+int Left = 4;
+int Off = 5; 
+int val;
+
+void setup() {
+  BTSerial.begin(38400);
+  Serial.begin(9600);
+  motors.setRightSpeed(0);
+  motors.setLeftSpeed(0);
+  BTSerial.listen();
+   
+  if(BTSerial.available() > 0 ){ 
+     val = BTSerial.read();  
+  }
+}
+
+void loop() {
+    while (Back == val){
+      for (int speed = 0; speed >= -400; speed--){
+          motors.setRightSpeed(speed);
+          motors.setLeftSpeed(speed);        
+       }
+       if(BTSerial.available() > 0){
+        val =  BTSerial.read();
+       }   
+    }
+    
+    while (Forward == val){
+      for (int speed = 0; speed <= 400; speed++){
+          motors.setRightSpeed(speed);
+          motors.setLeftSpeed(speed);          
+       }
+       if(BTSerial.available() > 0){
+        val =  BTSerial.read();
+       }  
+    }
+    
+     while (Left == val){
+       for (int speed = 0; speed <= 400; speed++){
+          motors.setRightSpeed(speed);
+          motors.setLeftSpeed(0);                 
+       }
+       if(BTSerial.available() > 0){
+        val =  BTSerial.read();
+       } 
+    }
+    
+     while (Right == val){
+       for (int speed = 0; speed <= 400; speed++){
+          motors.setLeftSpeed(speed);
+          motors.setRightSpeed(0);          
+       }
+       if(BTSerial.available() > 0){
+        val =  BTSerial.read();
+       }
+     }
+     
+     while (Off == val){
+      for (int speed = 0; speed <= 400; speed++){
+       motors.setLeftSpeed(0);
+       motors.setRightSpeed(0); 
+      }  
+       if(BTSerial.available() > 0){
+          val =  BTSerial.read();
+       }  
+     }
+      if(BTSerial.available() > 0){
+        val =  BTSerial.read();
+      }
+
+     motors.setRightSpeed(0);
+     motors.setLeftSpeed(0);
+}
+
---
+++ TransmitterBoi.ino
@@ -0,0 +1,84 @@
+//this is master
+#include <Adafruit_Sensor.h>
+#include <Adafruit_MMA8451.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(10, 11);
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+byte TiltBoi = 0;
+int sendinfo;
+
+void setup() 
+{
+  BTSerial.begin(38400);
+
+  Serial.begin(38400);
+  if (! mma.begin()){
+    Serial.println("Couldnt start");
+    while(1);
+  }
+  Serial.println("MMA8451 found!");
+  mma.setRange(MMA8451_RANGE_2_G);
+}
+
+void loop()
+{  
+  mma.read();
+   
+  sensors_event_t event; 
+  mma.getEvent(&event);
+  uint8_t o = mma.getOrientation();
+
+
+   switch (o) {
+    case MMA8451_PL_PUF: 
+      Serial.println(" Back ");
+      sendinfo = 1;
+      break;
+    case MMA8451_PL_PDF: 
+      Serial.println("Forward ");
+      sendinfo = 2;
+      break;
+    case MMA8451_PL_LRF: 
+      Serial.println("Right ");
+      sendinfo = 3;
+      break;
+    case MMA8451_PL_LLF: 
+      Serial.println("Left ");
+      sendinfo = 4 ;
+      break;
+    case MMA8451_PL_LLB: 
+      Serial.println("Tilt");
+      TiltBoi = !TiltBoi;
+      Serial.println(TiltBoi);
+      break;
+    case MMA8451_PL_PUB: 
+      Serial.println("Tilt");
+      TiltBoi = !TiltBoi;
+      Serial.println(TiltBoi);
+      break;    
+    case MMA8451_PL_PDB: 
+      Serial.println("Tilt");
+      TiltBoi = !TiltBoi;
+      Serial.println(TiltBoi);
+      break;
+    case MMA8451_PL_LRB: 
+      Serial.println("Tilt");
+      TiltBoi = !TiltBoi;
+      Serial.println(TiltBoi);
+      break;
+    }
+   Serial.println();
+
+   if (TiltBoi){
+      BTSerial.write(sendinfo);
+   }
+   else{
+     sendinfo = 5;
+     BTSerial.write(sendinfo);  
+   }
+delay(1000);
+}
+
